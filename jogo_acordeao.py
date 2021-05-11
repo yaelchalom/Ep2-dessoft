55598cd76694da0de1524fe44b026466aadf7a0f
Henrique Alves <riqalves11@gmail.com>
2021-05-11 19:20:05 -0300

    parte 1 arrumada + 2a parte Henrique

--- jogo_acordeao.py
+++ jogo_acordeao.py
@@ -18,4 +18,23 @@ while aperta == True:
             ind = lista_n[i]
             cart = baralho[i]
             print('{0}. {1}'.format(ind, cart))
+         a = input('Escolha uma carta (digite um número entre 1 e {}):)' .format(max(lista_n)))
+        while int(a) not in range(1, (max(lista_n)+1)):
+            a = input('Posição inválida. Digite um número entre 1 e {}, por favor): '.format(max(lista_n)))
+        indice = int(a) - 1
+    l   ista_movimento = todas_as_funções.lista_movimentos_possiveis(baralho, indice)
+        carta_selecionada = baralho[indice]
+        while lista_movimento == []:
+            a = int(input('Movimento não pode ser realizado, insire um número e 1 a {}: '.format(max(lista_movimento))))
+            if 0 <= a < max(lista_n):
+                break
+            mover = todas_as_funções.possui_movimentos_possiveis(baralho)
+            if mover == False:
+                iiii = 1
+                break
+            indice = int(a)-1
+            lista_movimento = todas_as_funções.lista_movimentos_possiveis(baralho, indice)
+        if iiii == 1:
+            break
+        
         
