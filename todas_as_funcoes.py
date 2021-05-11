2c9c6a0b7fde65c0e264ee679640a8c1079cc33e
Yael <yaelrkc@al.insper.edu.br>
2021-05-11 19:07:55 -0300

    ultimas 3 funções Yael

--- todas_as_funcoes.py
+++ todas_as_funcoes.py
@@ -13,4 +13,40 @@ def extrai_valor(carta):
         return '10'
     return carta[0]
     
-    
+def lista_movimentos_possiveis(baralho, indice):
+   lista_naipe = []
+   lista_valor = []
+   resultado = []
+   for i in range(len(baralho)):
+       naipe = extrai_naipe(baralho[i])
+       valor = extrai_valor(baralho[i])
+       lista_naipe.append(naipe)
+       lista_valor.append(valor)
+   if indice == 0:
+        return resultado
+   else:
+       if lista_valor[indice] == lista_valor[indice-1] or lista_naipe[indice] == lista_naipe[indice-1]:
+           resultado.append(1)
+       if indice > 2:
+           if lista_valor[indice] == lista_valor[indice-3] or lista_naipe[indice] == lista_naipe[indice-3]:
+               resultado.append(3)
+   return resultado
+
+def empilha(carta, indice, destino):
+    carta_selecionada = carta[indice]
+    carta[destino] = carta_selecionada
+    del(carta[indice])
+    return carta
+
+def possui_movimentos_possiveis(baralho):
+    existe = 0
+    for i in range(0,len(baralho)-1):
+        movimento = lista_movimentos_possiveis(baralho, i)
+        if movimento == []:
+            existe += 0
+        else:
+            existe += 1
+    if existe != 0:
+        return True
+    else:
+        return False
